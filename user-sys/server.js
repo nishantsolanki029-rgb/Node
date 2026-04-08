29f843b3b6f0b61db2d702a34faee6f4a320c9ff
nishantsolanki029-rgb <nishantsolanki029@gmail.com>
2026-04-08 19:32:56 +0530

    use-sys

---
+++ user-sys/server.js
@@ -0,0 +1,125 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const useModel = require("./Models/user.model");
+const postModel = require("./Models/post.model");
+const userModel = require("./Models/user.model");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+
+// fronted logic
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup");
+})
+
+app.get("/profile", auth, async (req,res)=>{
+    let user = await userModel.findOne({email: req.user.email})
+
+    res.render("profile", {user});
+})
+
+app.get("/post", auth,  (req, res)=>{
+    res.render("post");
+})
+
+
+// backend + database logic
+// dsignup user ==> create a new user
+app.post('/create', (req, res) => {
+
+    let { fullname, username, email, phone, image, password} = req.body;
+
+    bcrypt.hash(password, 10, async (err, hash) => {
+       try {
+        await userModel.create({
+            fullname,
+            username,
+            email,
+            phone,
+            password: hash,
+            image
+        });
+       } catch (err) {
+        console.log(err);
+       }
+    });
+    res.redirect("/")
+});
+
+// login user ==> check email and password, redirect to profile page
+app.post("/login", async (req,res)=>{
+   let user = await userModel.findOne({email: req.body.email});
+
+    if(!user.email){
+        res.send("Somthing Went Wrong - Email")
+    } else{
+        bcrypt.compare(req.body.password, user.password, (err, result)=>{
+            if(result){
+                let token = jwt.sign({email: user.email}, "aabbcc");
+                res.cookie("token", token);
+                res.redirect("/profile");
+            } else{ 
+                res.send("Somthing Went Wrong");
+            }
+        });
+    }
+});
+
+// logout user ==> remove token from cookie
+app.get("/logout", (req, res)=>{
+    res.clearCookie();
+    res.redirect("/");
+});
+
+// create post
+app.post('/post',auth, async(req, res) =>{
+    let user = await userModel.findOne({email: req.user.email});
+
+    let {title, description, imgurl} = req.body;
+
+   let createPost = await postModel.create({
+        userId: user._id,
+        title,
+        description,
+        imgurl,
+    });
+
+    // add (push) posts into user data
+    user.posts.push(createPost);
+    await user.save();
+
+    res.redirect("/profile");
+});
+
+// MiddleWare Functions
+function auth (req, res, next){
+    // console.log(req, res, next);
+    let token = req.cookies.token;
+    if(!token){
+        res.send("Access Denied !!");
+    }
+    try{
+        let verified = jwt.verify(token, "aabbcc");
+        req.user = verified // this data sent into rouute(check profile route, you can access email like req.user.email)
+
+        next();
+    } catch (error) {
+        console.log("Invalid Token");
+    }
+}
+
+app.listen(3000, () => {
+    console.log("✅ server is running");
+});
